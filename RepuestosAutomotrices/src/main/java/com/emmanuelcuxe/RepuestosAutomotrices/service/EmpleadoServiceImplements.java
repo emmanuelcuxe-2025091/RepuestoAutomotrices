f10787c5898b82bd33b799714d10f2cd9fcdecef
emmanuelcuxe-2025091 <ecuxe-2025091@Kinal.edu.gt>
2026-02-13 19:04:56 -0600

    feat: Se agrego el codigo de clase EmpleadoServiceImplements

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/EmpleadoServiceImplements.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/EmpleadoServiceImplements.java
@@ -1,4 +1,36 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.service;
 
-public class EmpleadoServiceImplements {
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Empleado;
+import com.emmanuelcuxe.RepuestosAutomotrices.repository.EmpleadoRepository;
+
+import java.util.List;
+
+public class EmpleadoServiceImplements implements EmpleadoService {
+    private final EmpleadoRepository empleadoRepository;
+
+    public EmpleadoServiceImplements(EmpleadoRepository empleadoRepository) {
+        this.empleadoRepository = empleadoRepository;
+    }
+
+    @Override
+    public List<Empleado> getAllEmpleados() { return empleadoRepository.findAll();
+    }
+
+    @Override
+    public Empleado getEmpleadoById(Integer id) { return empleadoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Empleado saveEmpleado(Empleado empleado) throws RuntimeException { return empleadoRepository.save(empleado);
+    }
+
+    @Override
+    public Empleado updateEmpleado(Integer id, Empleado empleado) throws RuntimeException{
+        empleado.setIdEmpleado(id);
+        return empleadoRepository.save(empleado);
+    }
+
+    @Override
+    public void deleteEmpleado(Integer id) {empleadoRepository.deleteById(id);
+    }
 }
