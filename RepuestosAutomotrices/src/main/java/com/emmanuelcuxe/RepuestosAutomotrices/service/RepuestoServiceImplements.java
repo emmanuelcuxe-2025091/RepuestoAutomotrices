4058ce74cacab0428888cfeb03a7bdc4e15a1f36
emmanuelcuxe-2025091 <ecuxe-2025091@Kinal.edu.gt>
2026-02-13 23:02:58 -0600

    feat: agregar métodos CRUD en RepuestoServiceImplements

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/RepuestoServiceImplements.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/RepuestoServiceImplements.java
@@ -1,4 +1,39 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.service;
 
-public class RepuestoServiceImplements {
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Repuesto;
+import com.emmanuelcuxe.RepuestosAutomotrices.repository.RepuestoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RepuestoServiceImplements implements RepuestoService{
+
+    private final RepuestoRepository repuestoRepository;
+
+    public RepuestoServiceImplements(RepuestoRepository repuestoRepository) {
+        this.repuestoRepository = repuestoRepository;
+    }
+
+    @Override
+    public List<Repuesto> getAllRepuestos() { return repuestoRepository.findAll();
+    }
+
+    @Override
+    public Repuesto getRepuestoById(Integer id) { return repuestoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Repuesto saveRepuesto(Repuesto repuesto) throws RuntimeException { return repuestoRepository.save(repuesto);
+    }
+
+    @Override
+    public Repuesto updateRepuesto(Integer id, Repuesto repuesto) throws RuntimeException{
+        repuesto.setIdRepuesto(id);
+        return repuestoRepository.save(repuesto);
+    }
+
+    @Override
+    public void deleteRepuesto(Integer id) {repuestoRepository.deleteById(id);
+    }
 }
