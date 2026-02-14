cbb6b17ebca335877717c49d649e9fb925126000
emmanuelcuxe-2025091 <ecuxe-2025091@Kinal.edu.gt>
2026-02-13 19:52:47 -0600

    add: Se agrego la clase ProveedorRepository

---
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/repository/ProveedorRepository.java
@@ -0,0 +1,10 @@
+package com.emmanuelcuxe.RepuestosAutomotrices.repository;
+
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProveedorRepository extends JpaRepository<Proveedor,Integer> {
+
+}
