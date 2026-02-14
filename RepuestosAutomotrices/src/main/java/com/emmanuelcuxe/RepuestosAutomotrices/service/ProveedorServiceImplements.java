8aca2c8c727fe9c53dc2c02e90f4d78137296856
emmanuelcuxe-2025091 <ecuxe-2025091@Kinal.edu.gt>
2026-02-13 20:07:38 -0600

    feat: Se agrego el codigo de la clase ProveeedorServiceImplements

--- RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/ProveedorServiceImplements.java
+++ RepuestosAutomotrices/src/main/java/com/emmanuelcuxe/RepuestosAutomotrices/service/ProveedorServiceImplements.java
@@ -1,4 +1,38 @@
 package com.emmanuelcuxe.RepuestosAutomotrices.service;
 
-public class ProveedorServiceImplements {
+import com.emmanuelcuxe.RepuestosAutomotrices.entity.Proveedor;
+import com.emmanuelcuxe.RepuestosAutomotrices.repository.ProveedorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProveedorServiceImplements implements ProveedorService {
+    private final ProveedorRepository proveedorRepository;
+
+    public ProveedorServiceImplements(ProveedorRepository proveedorRepository) {
+        this.proveedorRepository = proveedorRepository;
+    }
+
+    @Override
+    public List<Proveedor> getAllProveedores() { return proveedorRepository.findAll();
+    }
+
+    @Override
+    public Proveedor getProveedorById(Integer id) { return proveedorRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Proveedor saveProveedor(Proveedor proveedor) throws RuntimeException { return proveedorRepository.save(proveedor);
+    }
+
+    @Override
+    public Proveedor updateProveedor(Integer id, Proveedor proveedor) throws RuntimeException{
+        proveedor.setIdProveedor(id);
+        return proveedorRepository.save(proveedor);
+    }
+
+    @Override
+    public void deleteProveedor(Integer id) {proveedorRepository.deleteById(id);
+    }
 }
